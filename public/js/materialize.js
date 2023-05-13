2009ac80d82768b4948d0c50e7b73fc8c785cea8
Clifford Morin <cemorin21@gmail.com>
2023-05-13 16:04:54 -0700

    made background homepage image follow mouse on desktop

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -1,32 +1,35 @@
 // side nav for modile view
-document.addEventListener('DOMContentLoaded', () => {
-  const elems = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", () => {
+  const elems = document.querySelectorAll(".sidenav");
   const instances = M.Sidenav.init(elems, window.options);
 });
 
 // Carousel
-document.addEventListener('DOMContentLoaded', () => {
-  const elems = document.querySelectorAll('.carousel');
+document.addEventListener("DOMContentLoaded", () => {
+  const elems = document.querySelectorAll(".carousel");
   const instances = M.Carousel.init(elems, document.options);
 });
 
 // Modal
 
 $(document).ready(() => {
-  $('.modal').modal();
+  $(".modal").modal();
 });
 
 // Audio
 function play() {
-  const audio = document.getElementById('pallet');
-  audio.volume = 0.10;
+  const audio = document.getElementById("pallet");
+  audio.volume = 0.1;
   audio.loop = true;
-  if (audio.paused) { audio.currentTime = 0; audio.play(); } else audio.pause();
-};
+  if (audio.paused) {
+    audio.currentTime = 0;
+    audio.play();
+  } else audio.pause();
+}
 
 // Autocomplete search bar
 const getCardNames = async () => {
-  const results = await fetch('https://api.pokemontcg.io/v2/cards');
+  const results = await fetch("https://api.pokemontcg.io/v2/cards");
   const data = await results.json();
   const cards = data.data;
   const cardNames = cards.map((card) => card.name);
@@ -37,8 +40,8 @@ const getCardNames = async () => {
   return nameMap;
 };
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const elems = document.querySelectorAll('input.autocomplete');
+document.addEventListener("DOMContentLoaded", async () => {
+  const elems = document.querySelectorAll("input.autocomplete");
   const instances = M.Autocomplete.init(elems, {
     data: await getCardNames(),
     minLength: 1,
@@ -47,86 +50,90 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 $(document).ready(() => {
-  $('#password-login, #password-signup').characterCounter();
+  $("#password-login, #password-signup").characterCounter();
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const elems = document.querySelectorAll('.collapsible');
+document.addEventListener("DOMContentLoaded", () => {
+  const elems = document.querySelectorAll(".collapsible");
   const instances = M.Collapsible.init(elems, document.options);
 });
 
 // Or with jQuery
 
 $(document).ready(() => {
-  $('.collapsible').collapsible();
+  $(".collapsible").collapsible();
 });
 
 function myFunction() {
-  const x = document.getElementById('toggle');
+  const x = document.getElementById("toggle");
 
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
+  if (x.style.display === "none") {
+    x.style.display = "block";
   } else {
-    x.style.display = 'none';
+    x.style.display = "none";
   }
 }
 
 // Floating Action Button
 
 $(document).ready(() => {
-  $('.btn1').floatingActionButton({
-    direction: 'left',
+  $(".btn1").floatingActionButton({
+    direction: "left",
     hoverEnabled: false,
   });
-  $('.btn2').floatingActionButton({
-    direction: 'top',
+  $(".btn2").floatingActionButton({
+    direction: "top",
   });
 });
 
 // Preloader for Pokedex Page
-$(window).on('load', () => {
-  $('.progress').delay(250).fadeOut();
+$(window).on("load", () => {
+  $(".progress").delay(250).fadeOut();
 });
 
 // homepage audio
 
 function play() {
-  const audio = document.getElementById('pallet');
+  const audio = document.getElementById("pallet");
   audio.volume = 0.25;
   audio.loop = true;
-  if (audio.paused) { audio.currentTime = 0; audio.play(); } else audio.pause();
+  if (audio.paused) {
+    audio.currentTime = 0;
+    audio.play();
+  } else audio.pause();
 }
 
-// reveal
+// reveal mouse magic on homepage
 
-$(document).ready(function() {
+$(document).ready(function () {
   var $magic = $(".magic"),
-      magicWHalf = $magic.width() / 2;
-  $(document).on("mousemove", function(e) {
-    $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
-  });
+    magicWHalf = $magic.width() / 2;
+
+  // Apply the mouse movement effect only on screens larger than 600px
+  if ($(window).width() > 600) {
+    $(document).on("mousemove", function (e) {
+      $magic.css({ left: e.pageX - magicWHalf, top: e.pageY - magicWHalf });
+    });
+  }
 });
 
 // pop out button
-$('.chat-button').click(function(){
-  $('.pop-up').addClass('open');
+$(".chat-button").click(function () {
+  $(".pop-up").addClass("open");
 });
 
-$('.pop-up #close').click(function(){
-  $('.pop-up').removeClass('open');
+$(".pop-up #close").click(function () {
+  $(".pop-up").removeClass("open");
 });
 
 // animated button
-var animateButton = function(e) {
-
+var animateButton = function (e) {
   e.preventDefault;
   //reset animation
-  e.target.classList.remove('animate');
-  
-  e.target.classList.add('animate');
-  setTimeout(function(){
-    e.target.classList.remove('animate');
-  },700);
+  e.target.classList.remove("animate");
+
+  e.target.classList.add("animate");
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+  }, 700);
 };
-
-
